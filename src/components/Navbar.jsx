583632c5ad1337a4faf63e9c3f3009ea5cece1b1
Olesia Shcherbenko <oshch96@gmail.com>
2025-01-09 15:53:49 +0100

    feature/add-main-content-section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return (
-      <h1>Navbar component here</h1>
+      <header>
+        <nav>
+          <img src="src/assets/reactjs-icon.png" alt="React logo" />
+          <span>ReactFacts</span>
+        </nav>
+      </header>
     )
   }
